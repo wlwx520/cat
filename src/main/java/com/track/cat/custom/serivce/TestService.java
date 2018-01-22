19b6054c3307beb6b1f8e3180415db7297a073d4
wenxue <wenxue@cloudwalk.cn>
2018-01-22 14:38:43 +0800

    just for test

--- src/main/java/com/track/cat/custom/serivce/TestService.java
+++ src/main/java/com/track/cat/custom/serivce/TestService.java
@@ -4,14 +4,15 @@ import java.util.Map;
 
 import com.track.cat.core.Invocation;
 import com.track.cat.core.Result;
+import com.track.cat.core.handler.HttpMethod;
 import com.track.cat.core.handler.annotation.Handler;
 import com.track.cat.core.handler.annotation.Service;
 import com.track.cat.core.handler.interfaces.IService;
 import com.track.cat.core.http.ResultBuilder;
 
 @Service("/test")
-public class TestService implements IService{
-	@Handler("/aaa")
+public class TestService implements IService {
+	@Handler(value = "/aaa", method = { HttpMethod.POST, HttpMethod.GET })
 	public Result test(Invocation invocation) {
 		Map<String, String> data = invocation.getAttachment(Invocation.REQUEST);
 		System.out.println("in -----> " + data.toString());
