6e228b8efe8169eed370213fc23973b5d3ce602a
wenxue <wenxue@cloudwalk.cn>
2018-01-22 15:13:10 +0800

    添加file的测试

--- src/main/java/com/track/cat/custom/serivce/TestService.java
+++ src/main/java/com/track/cat/custom/serivce/TestService.java
@@ -1,6 +1,7 @@
 package com.track.cat.custom.serivce;
 
 import java.util.Map;
+import java.util.Set;
 
 import com.track.cat.core.Invocation;
 import com.track.cat.core.Result;
@@ -10,12 +11,31 @@ import com.track.cat.core.handler.annotation.Service;
 import com.track.cat.core.handler.interfaces.IService;
 import com.track.cat.core.http.ResultBuilder;
 
+import io.vertx.ext.web.FileUpload;
+
 @Service("/test")
 public class TestService implements IService {
-	@Handler(value = "/aaa", method = { HttpMethod.POST, HttpMethod.GET })
+	@Handler(value = "/text", method = { HttpMethod.POST, HttpMethod.GET })
 	public Result test(Invocation invocation) {
 		Map<String, String> data = invocation.getAttachment(Invocation.REQUEST);
 		System.out.println("in -----> " + data.toString());
 		return ResultBuilder.buildResult(0);
+	}
+
+	@Handler(value = "/file", method = { HttpMethod.FILE })
+	public Result test2(Invocation invocation) {
+		Set<FileUpload> fileUploads = invocation.getAttachment(Invocation.UPLOAD_FILES);
+		fileUploads.forEach(fileUpload->{
+			String contentType = fileUpload.contentType();
+			String fileName = fileUpload.fileName();
+			long size = fileUpload.size();
+			String uploadedFileName = fileUpload.uploadedFileName();
+			System.out.println(contentType);
+			System.out.println(fileName);
+			System.out.println(size);
+			System.out.println(uploadedFileName);
+		});
+
+		return null;
 	}
 }
